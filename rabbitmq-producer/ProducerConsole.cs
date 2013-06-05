11e5327b964f82a7029f92cdb6d2e996860f1a86
Sekhar Panja <sekharpanja@users.noreply.github.com>
2013-06-05 17:32:27 +0100

    fix dispose bug

--- rabbitmq-producer/ProducerConsole.cs
+++ rabbitmq-producer/ProducerConsole.cs
@@ -13,20 +13,18 @@ namespace rabbitmq_producer
         static void Main(string[] args)
         {
             //create the producer
-            using (_producer = new Producer(HostName, ExchangeName))
-            {
-                Console.WriteLine("Enter message to send:");
+            _producer = new Producer(HostName, ExchangeName);
+            Console.WriteLine("Enter message to send:");
 
-                while (true)
+            while (true)
+            {
+                string line = Console.ReadLine(); // Get string from user
+                if (line == "exit") // Check string
                 {
-                    string line = Console.ReadLine(); // Get string from user
-                    if (line == "exit") // Check string
-                    {
-                        break;
-                    }
-                    byte[] responseBytes = _producer.Get(System.Text.Encoding.UTF8.GetBytes(line), "rpc_test");
-                    Console.WriteLine(System.Text.Encoding.UTF8.GetString(responseBytes));
+                    break;
                 }
+                byte[] responseBytes = _producer.Get(System.Text.Encoding.UTF8.GetBytes(line), "rpc_test");
+                Console.WriteLine(System.Text.Encoding.UTF8.GetString(responseBytes));
             }
         }
 
