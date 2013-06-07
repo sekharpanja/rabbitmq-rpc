6ea899702242f710929fd46b01c1c915a036426d
Sekhar Panja <sekharpanja@users.noreply.github.com>
2013-06-07 10:08:15 +0100

    version 4

--- rabbitmq.library/SampleRpcServer.cs
+++ rabbitmq.library/SampleRpcServer.cs
@@ -8,9 +8,7 @@ namespace rabbitmq.library
 {
     public class SampleRpcServer : SimpleRpcServer
     {
-        public SampleRpcServer(Subscription subscription) : base(subscription)
-        {
-        }
+        public SampleRpcServer(Subscription subscription) : base(subscription) {}
 
         public override byte[] HandleSimpleCall(bool isRedelivered, IBasicProperties requestProperties, byte[] body, out IBasicProperties replyProperties)
         {
@@ -33,28 +31,18 @@ namespace rabbitmq.library
             {
                 // It's a request.
 
-                PublicationAddress replyAddress = PublicationAddress.Parse(properties.ReplyTo);
-                if (replyAddress == null)
-                {
-                    replyAddress = new PublicationAddress(ExchangeType.Direct,
-                                                          "",
-                                                          properties.ReplyTo);
-                }
+                PublicationAddress replyAddress = PublicationAddress.Parse(properties.ReplyTo) ??
+                                                  new PublicationAddress(ExchangeType.Direct, string.Empty, properties.ReplyTo);
 
                 IBasicProperties replyProperties;
-                byte[] reply = HandleCall(evt.Redelivered,
-                                          properties,
-                                          evt.Body,
-                                          out replyProperties);
+                byte[] reply = HandleCall(evt.Redelivered, properties, evt.Body, out replyProperties);
                 if (replyProperties == null)
                 {
                     replyProperties = m_subscription.Model.CreateBasicProperties();
                 }
 
                 replyProperties.CorrelationId = properties.CorrelationId;
-                m_subscription.Model.BasicPublish(replyAddress,
-                                                  replyProperties,
-                                                  reply);
+                m_subscription.Model.BasicPublish(replyAddress, replyProperties, reply);
             }
             else
             {
@@ -62,34 +50,6 @@ namespace rabbitmq.library
                 HandleCast(evt.Redelivered, properties, evt.Body);
             }
         }
-
-        //public override void ProcessRequest(BasicDeliverEventArgs evt)
-        //{
-        //    IBasicProperties properties = evt.BasicProperties;
-        //    if (!string.IsNullOrEmpty(properties.ReplyTo))
-        //    {
-        //        // It's a request.
-
-        //        PublicationAddress replyAddress = PublicationAddress.Parse(properties.ReplyTo) ??
-        //                                          new PublicationAddress(ExchangeType.Direct, "rpcExchange",
-        //                                                                 properties.ReplyTo);
-
-        //        IBasicProperties replyProperties;
-        //        byte[] reply = HandleCall(evt.Redelivered, properties, evt.Body, out replyProperties);
-        //        if (replyProperties == null)
-        //        {
-        //            replyProperties = m_subscription.Model.CreateBasicProperties();
-        //        }
-
-        //        replyProperties.CorrelationId = properties.CorrelationId;
-        //        m_subscription.Model.BasicPublish(replyAddress, replyProperties, reply);
-        //    }
-        //    else
-        //    {
-        //        // It's an asynchronous message.
-        //        HandleCast(evt.Redelivered, properties, evt.Body);
-        //    }
-        //}
 
     }
 }
